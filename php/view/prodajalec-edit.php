e06c3866d64af5fc3fd3d9c9b6143098429512d3
Pere98 <97481933+Pere98@users.noreply.github.com>
2022-01-10 23:34:27 +0100

    Create prodajalec-edit.php

---
+++ php/view/prodajalec-edit.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
+<meta charset="UTF-8" />
+<title>Edit User</title>
+
+<h1>Edit User</h1>
+
+<?php include("view/menu-links.php"); ?>
+
+<form action="<?= BASE_URL . "prodajalec/edit" ?>" method="post">
+	<p>
+        <label>ID: <span class="important"><?= $errors["id"] ?></span><br />
+		<textarea name="id" rows="1" cols="10"><?= $izdelek["id"] ?></textarea></label>
+	</p>
+    <p>
+        <label>Username: <span class="important"><?= $errors["username"] ?></span><br />
+		<textarea name="username" rows="1" cols="10"><?= $izdelek["username"] ?></textarea></label>
+	</p>
+    <p>
+        <label>Password: <span class="important"><?= $errors["password"] ?></span><br />
+		<textarea name="password" rows="1" cols="10"><?= $izdelek["password"] ?></textarea></label>
+	</p>
+    <p>
+        <label>Ime: <span class="important"><?= $errors["ime"] ?></span><br />
+		<textarea name="ime" rows="1" cols="10"><?= $izdelek["ime"] ?></textarea></label>
+	</p>
+            <p>
+        <label>Priimek: <span class="important"><?= $errors["priimek"] ?></span><br />
+		<textarea name="priimek" rows="1" cols="10"><?= $izdelek["priimek"] ?></textarea></label>
+	</p>
+            <p>
+        <label>Email: <span class="important"><?= $errors["email"] ?></span><br />
+		<textarea name="email" rows="1" cols="10"><?= $izdelek["email"] ?></textarea></label>
+	</p>
+    <p><button>Edit</button></p>
+</form>
