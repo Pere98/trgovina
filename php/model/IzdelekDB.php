51f5246d294114a4a24a21bed5df02a9c89fecb5
Pere98 <97481933+Pere98@users.noreply.github.com>
2022-01-11 20:02:04 +0100

    Update IzdelekDB.php

--- php/model/IzdelekDB.php
+++ php/model/IzdelekDB.php
@@ -7,7 +7,7 @@ class IzdelekDB {
     public static function getAll() {
         $db = DBInit::getInstance();
 
-        $statement = $db->prepare("SELECT id, ime, opis, lastnik, cena FROM izdelki");
+        $statement = $db->prepare("SELECT id, title, description, author, price FROM izdelki");
         $statement->execute();
 
         return $statement->fetchAll();
@@ -23,11 +23,11 @@ class IzdelekDB {
 
     public static function get($id) {
         $db = DBInit::getInstance();
-
-        $statement = $db->prepare("SELECT id, ime, opis, lastnik, cena FROM izdelki
-            WHERE id =:id");
+        //var_dump($id);
+        $statement = $db->prepare("SELECT id, title, description, author, price FROM izdelki WHERE id=:id");
         $statement->bindParam(":id", $id, PDO::PARAM_INT);
         $statement->execute();
+        //var_dump($statement->fetch());
 
         return $statement->fetch();
     }
@@ -35,7 +35,7 @@ class IzdelekDB {
     public static function insert($ime, $opis, $lastnik, $cena) {
         $db = DBInit::getInstance();
 
-        $statement = $db->prepare("INSERT INTO izdelki (ime, opis, lastnik, cena)
+        $statement = $db->prepare("INSERT INTO izdelki (title, description, author, price)
             VALUES (:ime, :opis, :lastnik, :cena)");
         $statement->bindParam(":ime", $ime);
         $statement->bindParam(":opis", $opis);
@@ -47,14 +47,18 @@ class IzdelekDB {
     public static function update($id, $ime, $opis, $lastnik, $cena) {
         $db = DBInit::getInstance();
 
-        $statement = $db->prepare("UPDATE izdelki SET ime = :ime, opis = :opis, "
-                . "lastnik = :lastnik, cena = :cena WHERE id =:id");
+        $statement = $db->prepare("UPDATE izdelki SET title = :ime, description = :opis, "
+                . "author = :lastnik, price = :cena WHERE id =:id");
         $statement->bindParam(":ime", $ime);
         $statement->bindParam(":opis", $opis);
         $statement->bindParam(":lastnik", $lastnik);
         $statement->bindParam(":cena", $cena);
         $statement->bindParam(":id", $id, PDO::PARAM_INT);
         $statement->execute();
+    }
+    
+    public static function getAllwithURI(array $prefix) {
+        return getAll();
     }
 
 }
